49fab62c1e3286747ab1456d8602de6f16077894
Lukas <lukas@klusis.com>
2020-12-27 01:46:07 +0200

    Added transformContent property support

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -2,6 +2,7 @@ import postcss, { AcceptedPlugin } from 'postcss'
 import postcssModules from 'postcss-modules'
 import postcssNested from 'postcss-nested'
 import postcssNesting from 'postcss-nesting'
+import prettier from 'prettier'
 
 import { plugin, TypeScriptDefinitionsPluginOptions } from './plugin'
 
@@ -158,6 +159,117 @@ it('works with nested classes (postcss-nesting)', async () => {
     }
 
     export default styles
+    "
+  `)
+  expect(result.warnings()).toHaveLength(0)
+})
+
+it('adds header', async () => {
+  const writeFile = jest.fn()
+  const { process } = getProcessor({
+    writeFile,
+    extra: { header: '/* eslint-disable */' },
+  })
+
+  const result = await process(`.a{}`)
+
+  expect(writeFile).toHaveBeenCalledTimes(1)
+  const { content } = writeFile.mock.calls[0][0]
+  expect(content).toMatchInlineSnapshot(`
+    "/* eslint-disable */
+    declare const styles: {
+      'a': string
+    }
+
+    export default styles
+    "
+  `)
+  expect(result.warnings()).toHaveLength(0)
+})
+
+it('adds footer', async () => {
+  const writeFile = jest.fn()
+  const { process } = getProcessor({
+    writeFile,
+    extra: { footer: '/* eslint-enable */' },
+  })
+
+  const result = await process(`.a{}`)
+
+  expect(writeFile).toHaveBeenCalledTimes(1)
+  const { content } = writeFile.mock.calls[0][0]
+  expect(content).toMatchInlineSnapshot(`
+    "declare const styles: {
+      'a': string
+    }
+
+    export default styles
+    /* eslint-enable */
+    "
+  `)
+  expect(result.warnings()).toHaveLength(0)
+})
+
+it('adds warning if "transformContent" returns falsy result', async () => {
+  const { process } = getProcessor({
+    writeFile: jest.fn(),
+    transformContent: () => '',
+  })
+
+  const result = await process(`.a{}`)
+  expect(result.warnings()).toHaveLength(1)
+})
+
+it('transform content', async () => {
+  const writeFile = jest.fn()
+  const { process } = getProcessor({
+    writeFile,
+    transformContent: (props) => JSON.stringify(props, undefined, 2),
+  })
+
+  const result = await process(`.a{}`)
+
+  expect(writeFile).toHaveBeenCalledTimes(1)
+  const { content } = writeFile.mock.calls[0][0]
+  expect(content).toMatchInlineSnapshot(`
+    "{
+      \\"paths\\": {
+        \\"cssFile\\": \\"/Users/lukasklusis/Development/lukaskl/postcss-typescript-d-ts/src\\",
+        \\"dtsFile\\": \\"/Users/lukasklusis/Development/lukaskl/postcss-typescript-d-ts/src.d.ts\\"
+      },
+      \\"parts\\": {
+        \\"classes\\": [
+          \\"a\\"
+        ]
+      },
+      \\"content\\": \\"declare const styles: {\\\\n  'a': string\\\\n}\\\\n\\\\nexport default styles\\\\n\\"
+    }"
+  `)
+  expect(result.warnings()).toHaveLength(0)
+})
+
+it('transform content (real life example)', async () => {
+  const writeFile = jest.fn()
+  const { process } = getProcessor({
+    writeFile,
+    transformContent: (props) =>
+      prettier.format(props.content, {
+        parser: 'typescript',
+        singleQuote: true,
+      }),
+  })
+
+  const result = await process(`.a{} .a-b{}`)
+
+  expect(writeFile).toHaveBeenCalledTimes(1)
+  const { content } = writeFile.mock.calls[0][0]
+  expect(content).toMatchInlineSnapshot(`
+    "declare const styles: {
+      a: string;
+      'a-b': string;
+    };
+
+    export default styles;
     "
   `)
   expect(result.warnings()).toHaveLength(0)
--- src/plugin.ts
+++ src/plugin.ts
@@ -4,6 +4,7 @@ import { defaultWriteFile, getClasses, WriteFileProps } from './utils'
 
 export interface TypeScriptDefinitionsPluginOptions {
   writeFile?: (props: WriteFileProps) => Promise<void> | void
+  transformContent?: (props: WriteFileProps) => Promise<string> | string
   extra?: {
     header?: string
     footer?: string
@@ -13,16 +14,19 @@ export interface TypeScriptDefinitionsPluginOptions {
 export const plugin = (
   opts: TypeScriptDefinitionsPluginOptions = {}
 ): Plugin => {
-  const { writeFile = defaultWriteFile, extra }: typeof opts = opts
+  const {
+    transformContent = (x) => x.content,
+    writeFile = defaultWriteFile,
+    extra,
+  }: typeof opts = opts
   const cache: { [path: string]: Set<string> } = {}
 
   return {
     postcssPlugin: 'postcss-typescript-d-ts',
-    Once: (root) => {
+    Once: (root, { result }) => {
       const file = root.source?.input.file
       if (!file) {
-        root.warn(
-          root.toResult(),
+        result.warn(
           `Filepath for the plugin "postcss-typescript-d-ts" was expected, however "${JSON.stringify(
             file
           )}" was received.`
@@ -32,7 +36,7 @@ export const plugin = (
       cache[file] = new Set<string>()
     },
 
-    OnceExit: (root) => {
+    OnceExit: async (root, { result }) => {
       const file = root.source?.input.file
       if (!file) {
         // warning was already outputted by the `Once` event.
@@ -40,8 +44,7 @@ export const plugin = (
       }
       const set = cache[file]
       if (!set) {
-        root.warn(
-          root.toResult(),
+        result.warn(
           `There is a bug in "postcss-typescript-d-ts" plugin, set of classes expected to be truthy but it was falsy.`
         )
       }
@@ -59,18 +62,28 @@ export const plugin = (
         '}',
         '',
         'export default styles',
-        '',
       ].join('\n')
 
-      const content = [extra?.header, classesDefinition, extra?.footer]
-        .filter((x) => !!x)
-        .join('\n')
-
-      writeFile({
-        content,
+      const contentProps = {
         paths: { cssFile: file, dtsFile: file + '.d.ts' },
         parts: { ...extra, classes },
-      })
+        content: [extra?.header, classesDefinition, extra?.footer, '']
+          .filter((x) => x !== undefined)
+          .join('\n'),
+      }
+
+      const writeFileProps = {
+        ...contentProps,
+        content: await transformContent(contentProps),
+      }
+      if (!writeFileProps.content) {
+        result.warn(
+          `Received content passed through user defined "transformContent" function is falsy. ` +
+            `Check if "transformContent" property is defined correctly.`
+        )
+      }
+
+      writeFile(writeFileProps)
     },
 
     Rule: async (rule, { result }) => {
@@ -81,8 +94,7 @@ export const plugin = (
       }
       const set = cache[file]
       if (!set) {
-        rule.warn(
-          result,
+        result.warn(
           `There is a bug in "postcss-typescript-d-ts" plugin, set of classes expected to be truthy but it was falsy.`
         )
       }
